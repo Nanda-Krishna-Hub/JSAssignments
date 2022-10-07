a3b5c05ad3662fa75a5432448568060e4d39126a
Nanda-Krishna-Hub <74372675+Nanda-Krishna-Hub@users.noreply.github.com>
2022-10-07 23:42:47 +0530

    Add files via upload

---
+++ Assignment-4_MatchHouse.js
@@ -0,0 +1,9 @@
+function matchHouse(n){
+    return 6*n-(n - 1);
+}
+// for 1 step it takes 6 match sticks
+// for 2 steps it takes 6*2-1
+// for 3 steps it takes 6*3-2
+// for n steps it takes 6*n-(n-1)
+ 
+console.log(matchHouse(87));
