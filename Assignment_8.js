9971a7b0800601b72e37deb1ff5cacff92e0ae72
Nanda-Krishna-Hub <74372675+Nanda-Krishna-Hub@users.noreply.github.com>
2022-10-21 07:21:34 +0530

    Add files via upload

---
+++ Assignment_8.js
@@ -0,0 +1,28 @@
+//        1.
+
+            //  No we cannot put duplicate values in the set 
+            //    we can only put unique element in set
+
+
+            // 2. Syntax for 
+
+                // a. 
+                    // let set = new Set();
+                    // set.add(8)
+                    // set.add(6)
+                    // console.log(set)
+                    // set.delete(8)
+                    // console.log(set)
+                    
+
+            // 3.
+                    // let set = new Set();
+                    // set.add(3)
+                    // set.add(5)
+                    // set.add(0)
+                    // set.add(6)
+                    // console.log(set)
+                    // console.log(set.has(8))
+                    
+
+
